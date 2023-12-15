816e6a4d11ae55165c5dc4cf52850586e7d88309
RMLemos <143395332+RMLemos@users.noreply.github.com>
2023-12-15 18:52:10 +0000

    Initial commit

---
+++ website/models.py
@@ -0,0 +1,21 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(2000), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    due_date = db.Column(db.DateTime(timezone=True),  nullable=True)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    date_updated = db.Column(db.DateTime(timezone=True),  nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    todos = db.relationship('Todo')
+
+
---
+++ website/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, request, flash, redirect
+from flask_login import login_required, current_user
+from datetime import datetime
+from .models import Todo
+from . import db
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        input_content = request.form.get('input_content')
+        input_due_date = request.form.get('input_due_date')
+        due_date = datetime.strptime(input_due_date, '%Y-%m-%d')
+        if len(input_content) < 3:
+            flash('Task is to short!', category='error')
+        else:
+            new_task = Todo(content=input_content, due_date=due_date, user_id=current_user.id)
+            db.session.add(new_task) #adding the note to the database 
+            db.session.commit()
+            return redirect('/')
+    
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%B, %d %Y")
+    return render_template("index.html", user=current_user, formatted_date=formatted_date)
+
