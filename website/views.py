08821b593e2c9560fc4cb6901f8e47a22fdec773
RMLemos <143395332+RMLemos@users.noreply.github.com>
2023-12-16 19:16:01 +0000

    search form completed

--- website/views.py
+++ website/views.py
@@ -6,25 +6,38 @@ from . import db
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST':
+        # Se for uma busca, processar a pesquisa
+        search_query = request.form.get('search_query')
+        if search_query:
+            results = Todo.query.filter(Todo.content.ilike(f"%{search_query}%")).all()
+            return render_template('index.html', tasks=results, user=current_user, search_query=search_query)
+       
+    if request.method == 'POST':
+        
+        # Se não for uma busca, processar adição de tarefa
         input_content = request.form.get('input_content')
         input_due_date = request.form.get('input_due_date')
-        due_date = datetime.strptime(input_due_date, '%Y-%m-%d')
-        if len(input_content) < 3:
-            flash('Task is to short!', category='error')
-        else:
-            new_task = Todo(content=input_content, due_date=due_date, user_id=current_user.id)
-            db.session.add(new_task) #adding the note to the database 
-            db.session.commit()
-            #return redirect('/')
-    else:
-        today = date.today()
-        tasks = Todo.query.filter(db.func.date(Todo.due_date) == today).order_by(Todo.due_date).all()
+
+        if input_content is not None:
+            if input_content and input_due_date:
+                due_date = datetime.strptime(input_due_date, '%Y-%m-%d')
+                new_task = Todo(content=input_content, due_date=due_date, user_id=current_user.id)
+                db.session.add(new_task)
+                db.session.commit()
+                flash('Task added!', category='success')
+            else:
+                flash('Must add a task and a due date!', category='error')
+        
+    # Lógica para exibir tarefas de hoje
+    today = date.today()
+    tasks = Todo.query.filter(db.func.date(Todo.due_date) == today).order_by(Todo.due_date).all()
+
     current_date = datetime.now()
     formatted_date = current_date.strftime("%B, %d %Y")
     return render_template("index.html", user=current_user, formatted_date=formatted_date, tasks=tasks)
+
 
